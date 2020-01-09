474385580ff5cad1570e89a354c01b5342e82d24
jemmeli <jemmeli84@gmail.com>
2020-01-09 22:26:27 +0100

    authentification method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+	before_action :set_user, only: [:edit, :update, :show]
+
 	def new
 		@user = User.new
 	end
@@ -12,7 +14,7 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
+		#@user = User.find(params[:id])
 		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 
@@ -27,11 +29,11 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		#@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
+		#@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:success] = "Your account was updated successfully"
 			redirect_to articles_path
@@ -45,6 +47,10 @@ class UsersController < ApplicationController
 
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
 	end
 
 end
