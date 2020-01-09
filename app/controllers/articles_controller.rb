44cb2dd6f347dc8e025d5e277bb6e258725ed07a
jemmeli <jemmeli84@gmail.com>
2020-01-09 03:32:35 +0100

    add signup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 	def create
 		#debugger
 		#render plain: params[:article].inspect
-		#@article.user = User.first
+		@article.user = User.first
 		@article = Article.new(article_params)
 		if @article.save
 			flash[:success] = "Article was successfully created"
